abb965d700c6374cb9c10638b8bc6b5c8139613a
Ольга <116906470+olenka-git-hub@users.noreply.github.com>
2022-12-11 22:44:59 +0500

    Рандом

--- 1.4/Program.cs
+++ 1.4/Program.cs
@@ -1,3 +1,4 @@
 ﻿double NumberA =12;
 double NumberB = 5;
+int result = NumberA + NumberB;
 Console.WriteLine (NumberA / NumberB);
---
+++ 1.5/Program.cs
@@ -0,0 +1,6 @@
+﻿int NumberA = new Random().Next (1, 10);
+Console.WriteLine(NumberA);
+int NumberB = new Random ().Next (1, 10);
+Console.WriteLine (NumberB);
+int result = NumberA + NumberB;
+Console.WriteLine (result);
